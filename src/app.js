906e457052ac8d6637e569d835bee68fc09fb570
Bruna <bruna.dall9741@gmail.com>
2022-09-21 20:29:17 -0300

    Projeto 2: Clone do Repositório da Entrega e Criação do Server

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ app.use(express.json());
     
 const PORT = process.env.PORT || 3000   
 
+app.get('/', (req, res) => {
+    return res
+    .status(200)
+    .json({
+        message: "está funcionando"
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`)
 });
