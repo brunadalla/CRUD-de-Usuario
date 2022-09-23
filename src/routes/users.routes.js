807dbc0a395069d74b821f825c968cdac2af5875
Bruna <bruna.dall9741@gmail.com>
2022-09-23 20:54:28 -0300

    feat: user profile

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,10 +6,13 @@ import listUserController from "../controllers/listUsers.controller"
 import verifyEmailAvailabilityMiddleware from "../middleware/verifyEmailAvailability.middleware"
 import verifyAdmMiddleware from "../middleware/verifyAdm.middleware"
 import verifyAuthTokenMiddleware from "../middleware/verifyAuthToken.middleware"
+import profileController from "../controllers/profile.controller"
 
 const router = Router()
 
 router.post("", verifyEmailAvailabilityMiddleware, createUserController)
 router.get("", verifyAuthTokenMiddleware, verifyAdmMiddleware, listUserController)
+
+router.get("/profile", verifyAuthTokenMiddleware, profileController)
 
 export default router
---
+++ src/services/profile.service.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken" 
+
+import users from "../database"
+
+const profileService = (token) => {
+    const decodedToken = jwt.decode(token, {complete: true})
+    const userId = decodedToken.payload.id
+
+    const user = users.find(user => user.id === userId)
+
+    return user
+}
+
+export default profileService
